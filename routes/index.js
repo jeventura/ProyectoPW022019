1a9ef4e47f7290c75a5b3927c9e6c52824cf09f0
RoxanaCortez <00502211@uca.edu.sv>
2019-11-14 12:16:23 -0600

    add: user editado

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sadmin.jade');
+  res.render('user.jade');
 });
 
 module.exports = router;
