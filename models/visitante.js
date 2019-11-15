1d17df429d85faa8ac0fbf45b9175f5485caf8e5
julio ventura <je.ventura@gmail.com>
2019-11-15 14:47:35 -0600

    Añadiendo validacion de placas y modelo de usuario

--- models/visitante.js
+++ models/visitante.js
@@ -5,13 +5,13 @@ var VisitanteSchema = Schema({
     nombre: {
         type: String,
         required: true,
-        min: [3, "Introduzca un nombre completo"]
+        min: [3, "Introduzca un nombre completo"],
        
     },
     Documento: {
         type: Number,
-        required: true
-        unique:true
+        required: true,
+        unique:true,
         min:[9, "Introduzca documento completo"]
     },
     tipo: {
@@ -19,13 +19,13 @@ var VisitanteSchema = Schema({
     },
     Placa:{
         type: String,
-        required:true
+        required: false, validate: /[^PNOACVTFMDE][1-9]{1,6}|^(CD|PR|RE|AB|MI|MB)([0-9]{1,5})$|^(PNC)([0-9]{1,4})$/,
         min: [3, 'formato incorrecto'],
         max: 7
-    }
+    },
     Descripcion:{
         type: String,
-        required: true
+        required: true,
         min:[3, 'Incluya una descripcion']
 
     }
