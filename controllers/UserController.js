a58ce0163a871d769a16f2d4c199628e4dfe929d
danielachavez <00028116@uca.edu.sv>
2019-11-20 22:41:55 -0600

    Controlador visitante

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,63 +1,63 @@
-//obtener todo
+
 const User =require("./../models/Usuario");
 
-//insertar nuevos vistantes
+
 const insert =(req, res)=>{
     const usuario = new User(req.body);
     usuario.save ((error, documents)=>{
         if(error)
             return res.status(500).json({
-                msg:"hubo un error"
+                msg:"Hubo un error"
             });
         return res.status(201).json({
-            msg: "creado",
+            msg: "Creado",
             user:documents
         });
     });
 };
 
-//buscar por id
+
 const getOneUser= (req,res)=>{
     User.findById(req.params.id, (error, documents)=>{
         if(error)
             return res.status(500).json({
-                msg:"hubo un error"
+                msg:"Hubo un error"
             });
         return res.status(200).json({
-            msg:"ok",
+            msg:"Ok",
             users:documents
         });
     });
 }
 
-//funcion de todos los registros guardados en la base
+
 const getUser = (req,res)=>{
     User.find({}, (error, documents)=>{
         if(error)
             return res.status(500).json({
-                msg:"hubo un error"
+                msg:"Hubo un error"
             });
         return res.status(200).json({
-            msg:"ok",
+            msg:"Ok",
             users:documents
         });
     });
 };
 
-//Edita un elemento
+
 const update = (req, res)=>{
     let user = req.body
     
     if(!user._id){
         return res.status(400).json({
-            message: "id is needed",
+            message: "Id is needed",
         }); 
     }
 
     User.update({_id: user._id}, user)
         .then(value =>{
             res.status(200).json({
-                message: "update register was successful"
+                message: "Update register was successful"
             });
         })
         .catch((err)=>{
@@ -73,14 +73,14 @@ const deleteById = (req, res)=>{
 
     if(!user._id){
         return res.status(400).json({
-            message: "id is needed",
+            message: "Id is needed",
         }); 
     }
 
     User.deleteOne({_id:user._id})
         .then(deleted=>{
             res.status(200).json({
-                message: "delete register was successful"
+                message: "Delete register was successful"
             });
         })
         .catch(err=>{
