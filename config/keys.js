00d525a6c720de4d10af0aaf021f1215ce1d9213
julio ventura <je.ventura@gmail.com>
2019-11-27 23:54:10 -0600

    trabajando en las validaciones de login

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://je.ventura@gmail.com:'+ encodeURIComponent('M0nic@linda') + '@cluster0-tsqw4.mongodb.net/test?retryWrites=true';
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
